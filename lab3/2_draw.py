5976da0831bc3aba4f2f8f22d3c071b6bacb6435
Ivashkin Ivan <ivashkinir@yandex.ru>
2020-09-23 14:49:24 +0300

    Домашняя работа. Этап 2

--- lab3/2_draw.py
+++ lab3/2_draw.py
@@ -1,45 +1,63 @@
 import pygame
 from pygame.draw import *
+from pygame.font import *
 
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((927, 769))
 
-rect(screen, (255, 240, 245), (0, 0, 927, 769))
+#colors
+BLACK = (0, 0, 0)
+GREEN = (127, 255, 42)
+BLUE = (128, 179, 255)
+BROWN = (120, 68, 33)
+WHITE = (255, 255, 255)
+BEIGE = (233, 198, 175)
+DARK_ORANGE = (255, 102, 3)
+RED = (255, 42, 42)
 
-circle(screen, (210, 105, 30), (450, 820), 300)
-circle(screen, (255, 228, 196), (450, 400), 230)
+#screen
+width = 927
+height = 769
+screen = pygame.display.set_mode((width, height))
+screen.fill(WHITE)
 
-circle(screen, (255, 228, 196), (450, 400), 220)
-circle(screen, (255, 228, 196), (450, 400), 220)
+def eye(x, y, eye_width, eye_height):
+    ellipse(screen, BLUE, (x - eye_width//2, y - eye_height//2, eye_width, eye_height))
+    ellipse(screen, BLACK, (x - eye_width//2, y - eye_height//2, eye_width, eye_height), 1)
+    ellipse(screen, BLACK, (x - 17, y - 10, 34, 24))
 
-ellipse(screen, (135, 206, 250), (320, 320, 100, 90))
-ellipse(screen, (0, 0, 0), (320, 320, 100, 90), 1)
+def hand(x_st, y_st, x_ed, y_ed):
+    line(screen, BEIGE, [x_st, y_st], [x_ed, y_ed], 40)
 
-ellipse(screen, (135, 206, 250), (480, 320, 100, 90))
-ellipse(screen, (0, 0, 0), (480, 320, 100, 90), 1)
+#body
+circle(screen, DARK_ORANGE, (width//2, height), 300)
 
-ellipse(screen, (0, 0, 0), (515, 357, 30, 27))
-ellipse(screen, (0, 0, 0), (355, 357, 30, 27))
+#face
+circle(screen, BEIGE, (width//2, height//2), 230)
 
-line(screen, (255, 228, 196), [85, 50], [190, 570], 40)
-line(screen, (255, 228, 196), [800, 50], [710, 580], 40)
+#mouth
+polygon(screen, RED, [(width//2, height//2 + 140), (width//2 - 110, height//2 + 85), (width//2 + 110, height//2 + 85)])
+polygon(screen, BLACK, [(width//2, height//2 + 140), (width//2 - 110, height//2 + 85), (width//2 + 110, height//2 + 85)], 1)
 
-polygon(screen, (255, 0, 0), [(350, 480), (450, 545), (550, 480)])
-polygon(screen, (0, 0, 0), [(350, 480), (450, 545), (550, 480)], 1)
+#nose
+polygon(screen, BROWN, [(width//2, height//2 + 50), (width//2 - 30, height//2 + 20), (width//2 + 30, height//2 + 20)])
+polygon(screen, BLACK, [(width//2, height//2 + 50), (width//2 - 30, height//2 + 20), (width//2 + 30, height//2 + 20)], 1)
 
-polygon(screen, (139, 69, 19), [(410, 430), (450, 460), (490, 430)])
-polygon(screen, (0, 0, 0), [(410, 430), (450, 460), (490, 430)], 1)
+#eyes
+eye(width//2 - 75, height//2 - 50, 100, 90)
+eye(width//2 + 75, height//2 - 50, 100, 90)
 
-polygon(screen, (210, 105, 30), [(145, 575), (235, 535), (295, 605), (245, 680), (155, 665)])
-polygon(screen, (0, 0, 0), [(145, 575), (235, 535), (295, 605), (245, 680), (155, 665)], 1)
+#hands
+hand(width//2 - width//4, 3*height//4, 60, 20)
+hand(width//2 + width//4, 3*height//4, width - 60, 20)
 
-polygon(screen, (210, 105, 30), [(605, 605), (665, 535), (755, 575), (745, 665), (655, 680)])
-polygon(screen, (0, 0, 0), [(605, 605), (665, 535), (755, 575), (745, 665), (655, 680)], 1)
-
-rect(screen, (0, 255, 0), (0, 0, 927, 90))
-rect(screen, (0, 0, 0), (0, 0, 927, 90), 2)
+#table
+rect(screen, GREEN, (0, 0, width, 90))
+rect(screen, BLACK, (0, 0, width, 90), 1)
+my_font = pygame.font.Font(None, 126)
+text = my_font.render('PYTHON is AMAZING', 1, BLACK)
+screen.blit(text, (3, 3))
 
 pygame.display.update()
 clock = pygame.time.Clock()
